e28170fdf47e09b51c7d581ab12a1fe5e7411e8d
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 18:06:17 +0530

    repeated code

---
+++ 02-repeatedcode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Avinash : Hi Kumar, How are you doing!"
+echo "Kumar: Hello Avinash, I am good."
+echo "Avinash: How is your job!"
+echo "Kumar: not bad, I am planning to learn Devops with AWS"
