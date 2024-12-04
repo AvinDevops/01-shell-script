c84d0ed6c1e782915f7d03540bb9b0493b5c5e1e
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 22:19:25 +0530

    using conditions

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,9 @@
 
 Number=$1
 
-if [$Number -gt 10]
+if [ $Number -gt 10 ]
 then
     echo "Entered number $Number is greater than 10"
 else
-    eho "Entered number $Number is less than 10"
+    echo "Entered number $Number is less than 10"
 fi
