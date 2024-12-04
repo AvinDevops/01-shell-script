99d7e60f2b8ed9b075dbabc0e59c75a6f504b769
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 18:13:40 +0530

    using variables

---
+++ 03-usingvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Name1=Avinash
+Name2=Kumar
+
+echo "$Name1: Hi $Name2, How are you doing!"
+echo "$Name2: Hello $Name1, I am good."
+echo "$Name1: How is your job!"
+echo "$Name2: not bad, I am planning to learn Devops with AWS"
