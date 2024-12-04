6e9ae1545fe448ededd2eb66305c4b396672e1fc
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 18:21:33 +0530

    using variables

--- 03-usingvariables.sh
+++ 03-usingvariables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-${Name1}=$1   #Bandela we can pass as arguments/parameters
+Name1=$1   #Bandela we can pass as arguments/parameters
 Name2=$2    #Kativarapu
 
 echo "${Name1}: Hi $Name2, How are you doing!"
 echo "$Name2: Hello $Name1, I am good."
 echo "$Name1: How is your job!"
 echo "$Name2: not bad, I am planning to learn Devops with AWS"
+
+#only we can like this ${} while referring variable
