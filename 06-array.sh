d9e2e9c2b2b2d999e60e8745d84343243ed763d9
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 22:13:50 +0530

    using array

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Heros=("Ironman" "WarMachine" "Spiderman")
+
+echo "First super hero name is : ${Heros[0]}"
+echo "Second super hero is : ${Heros[1]}"
+echo "Third super hero is : ${Heros[2]}"
+echo "All heros names are : ${Heros[@]}"
