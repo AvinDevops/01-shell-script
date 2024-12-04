3e5f135a8dcef25a70ce5b14558bc1122cd75d97
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-04 22:28:40 +0530

    using special variables

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -3,3 +3,6 @@
 echo "All variables : $@"
 echo "Number of variables: $#"
 echo "current working directory: $PWD"
+echo "who is the user: $USER"
+echo "Hostname: $HOSTNAME"
+echo "current script name: $0"
