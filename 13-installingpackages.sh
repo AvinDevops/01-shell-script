9f12489d0f007fab40c781ede8ae4c3f58d756ee
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-05 16:44:08 +0530

    installing multiple packages

--- 13-installingpackages.sh
+++ 13-installingpackages.sh
@@ -5,7 +5,9 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOG=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
@@ -20,4 +22,11 @@ fi
 for i in $@
 do 
     echo $i
+    dnf list installed mysql
+    if [ $? -ne 0 ]
+    then
+        echo -e "$i is already installed...$G SKIPPING $N"
+    else
+        echo -e "$i is not installed...$R now install it"
+    fi
 done
