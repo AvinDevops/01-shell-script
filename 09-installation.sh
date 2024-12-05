d25a794d0c9cb574f3106b20aa4910a279a0c936
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-05 14:57:38 +0530

    if condition with exit status

--- 09-installation.sh
+++ 09-installation.sh
@@ -10,5 +10,12 @@ else
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "Mysql installation is filed"
+else
+    echo "Mysql installation is success"
+fi
+
 
 echo "Is script proceeding..?"
