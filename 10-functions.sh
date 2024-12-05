71052d02b8057efe5360f33b4610db8105490ece
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-05 15:32:24 +0530

    using functions

---
+++ 10-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "exit status: $1"
+    echo "what are you doing: $2"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please access with root user"
+    
+else
+    echo "you have root access, please install"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
