937f79d7255250b83e4c4cd9b2a98023e934254d
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-06 19:45:26 +0530

    script-1

--- 14-script1.sh
+++ 14-script1.sh
@@ -5,7 +5,11 @@ COURSE="Devops from script-1"
 echo "script-1 value, course : $COURSE "
 echo "Process ID of script-1 : $$"
 
-./15-script2.sh
+#./15-script2.sh
+source ./15-script2.sh
+
 
 echo "After calling script-2 : $COURSE "
 echo "Process ID of script-1 : $$ "
+
+
