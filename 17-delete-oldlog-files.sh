b2514085d092a053017b044cac2bdff65c4be59a
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-06 23:13:12 +0530

    using set -e and error handling

--- 17-delete-oldlog-files.sh
+++ 17-delete-oldlog-files.sh
@@ -4,11 +4,18 @@ SOURCE_DIRECTORY="/tmp/logs/"
 
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-for i in $FILES
-do 
-    echo $i
-done
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo "$SOURCE_DIRECTORY exists"
+else
+    echo "$SOURCE_DIRECTORY is not exixts"
+fi
 
+
+while IFS = read -r line
+do 
+    echo "Deleting file : $line"
+done <<< $FILES
 
 
 
