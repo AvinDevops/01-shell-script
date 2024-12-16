1e30f69bb9139eb9ddd0f32ba0db38c48527c344
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-16 18:48:46 +0530

    monitoring disk usage

---
+++ 18-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_LIMIT=6
+
+while IFS=read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}' )
+    if [ $USAGE -ge $DISK_LIMIT ]
+    then
+        MESSAGE="$FOLDER memory exceeds than disk limit $DISK_LIMIT, current usage: $USAGE"
+    fi
+
+
+done <<< $DISK_USAGE
